3133a48f2ea651fb898b888209a4f04753b63a30
윤은식 <developer.lisiver@gmail.com>
2020-07-03 17:19:46 +0900

    feat(store): redux store initialize in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,31 @@
 import '../styles/global.css'
-import { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// redux
+import withRedux from 'next-redux-wrapper';
+import { Provider } from 'react-redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import reducer from '../reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const App = ({ Component, store }) => {
+  return (
+    <Provider store={store}>
+      <Component />
+    </Provider>
+  );
+};
+
+const configureStore = (initialState, options) => {
+  const middlewares = []; // 미들웨어
+  const enhancer = process.env.NODE_ENV === 'production' ?
+    compose(applyMiddleware(...middlewares)) :
+    composeWithDevTools(
+      applyMiddleware(...middlewares)
+    );
+  const store = createStore(reducer, initialState, enhancer);
+
+  return store;
 }
+
+// @ts-ignore
+export default withRedux(configureStore)(App);
